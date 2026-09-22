fb208b687da1b2fe1e9616c03737d9ff278c476d
RajatKumar-git <62398114+RajatKumar-git@users.noreply.github.com>
2026-09-22 21:33:39 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+st.title('AT Traveller App') 
+destination=st.text_input("Enter destination: ")
+travel_date=st.date_input("Enter the travel Date:")
+budget=st.number_input(input("Enter budget:"))
+hotel_required=st.selectbox(input("Do you need hotel stay(Yes for 1/No for 0): "))
+
+if(st.button("submit"))
+st.write(print(f"""
+AI Travel Agent Summary
+-----------------------
+Destination \t: {destination}
+Travel Date \t: {travel_date}
+Budget  \t: {budget}
+Hotel_Required \t: {hotel_required}"""))
+st.balloons()
+
