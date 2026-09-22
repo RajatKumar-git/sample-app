f312cd5cb0ff2ba46941c9b9140a0ba753a4ab06
RajatKumar-git <62398114+RajatKumar-git@users.noreply.github.com>
2026-09-22 21:57:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ st.title('AT Traveller App')
 destination=st.text_input("Enter destination: ")
 travel_date=st.date_input("Enter the travel Date:")
 budget=st.number_input(input("Enter budget:"))
-hotel_required=st.selectbox(input("Do you need hotel stay(Yes for 1/No for 0): "))
+hotel_required=st.selectbox("Do you need hotel stay",("Yes","No"))
 
 if(st.button("submit")):
     st.write(print(f"""
