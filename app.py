6125f867778f7f325b92129bf7dc854845eb397e
RajatKumar-git <62398114+RajatKumar-git@users.noreply.github.com>
2026-09-23 19:40:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 import streamlit as st
-st.title('AT Traveller App')
+st.title('Smart AT Traveller App')
 destination=st.text_input("Enter destination: ")
 travel_date=st.date_input("Enter the travel Date:")
 budget=st.number_input("Enter budget:")
 hotel_required=st.selectbox("Do you need hotel stay",("Yes","No"))
+
+travel_type=st.selectbox(
+    "Travel Type",
+    ["Solo","Family","Couple","Business"]
+    )
+
+interest =st.multiselect(
+    "Interest",
+    ["Beach","Temple","Adventure","Nature"]
+)
 
 if st.button("submit"):
     st.write(print(f"""
